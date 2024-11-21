a41b9217528307b855c51f83cf4658c54c17327d
Giang-Davy <davy.dangvu@gmail.com>
2024-11-21 16:04:26 +0000

    ajout 3

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(97, 123):
+    if i == 113:
+        print(end="")
+    elif i == 101:
+        print(end="")
+    else:
+        lettre = "{}".format(chr(i))
+        print(lettre, end="")
