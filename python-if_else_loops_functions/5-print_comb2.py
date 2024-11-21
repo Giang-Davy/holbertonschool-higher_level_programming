932a12727eeb193b6bafd20ed46ff9d8dd8ede13
Giang-Davy <davy.dangvu@gmail.com>
2024-11-21 16:53:06 +0000

    ajout 5

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(100):
+    if i < 10:
+        resultat1 = "0{}".format(i)
+        print(resultat1, end=", " if i < 99 else "")
+    else:
+        resultat2 = "{}".format(i)
+        print(resultat2, end=", " if i < 99 else "")
