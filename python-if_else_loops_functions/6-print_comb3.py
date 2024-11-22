18047becdbb1d10c310f5efc9ac7dccbb36c8853
Giang-Davy <davy.dangvu@gmail.com>
2024-11-22 13:34:31 +0000

    6-V2

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for i in range(10):  # Premier chiffre de la combinaison
     for j in range(i + 1, 10):  # Deuxième chiffre, commence à i+1 pour éviter les doublons
-        # Formatage des combinaisons en deux chiffres
+        # Formatage des combinaisons en deux chiffres avec .format()
         if i == 8 and j == 9:  # Dernière combinaison, donc pas de virgule finale
-            print(f"{i}{j}")
+            print("{}".format(f"{i}{j}"))
         else:
-            print(f"{i}{j}", end=", ")
+            print("{}".format(f"{i}{j}"), end=", ")
 
