1ae8c8950b99e2cf61c28baae4e9b69ccb8e2ac2
Giang-Davy <davy.dangvu@gmail.com>
2024-11-25 16:06:58 +0000

    1-V6

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 """Representation d'un carre"""
 
+
 class Square:
-    """Définition de la classe Square"""
+    """définition de la classe Square"""
 
     def __init__(self, size):
-        """Initialisation de size"""
+        """initialisation de size"""
         self.__size = size
