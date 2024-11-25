258b4abf3b561d984d4d127ef240e02263b702fe
Giang-Davy <davy.dangvu@gmail.com>
2024-11-25 09:57:40 +0000

    8-V3

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,6 @@ def uppercase(str):
         if ord(char) >= 97 and ord(char) <= 122:
             resultat += chr(ord(char) - 32)
         else:
-            # Ajouter le caractère tel quel
             resultat += char
     print("{}".format(resultat))
 
