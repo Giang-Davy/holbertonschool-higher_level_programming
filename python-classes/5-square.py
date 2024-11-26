5cc6bebebee53cac682a4524b20a6f9628d574c1
Giang-Davy <davy.dangvu@gmail.com>
2024-11-26 13:18:47 +0000

    ajout 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""Representation d'un carré"""
+
+
+class Square:
+    """Définition de la classe Square"""
+
+    def __init__(self, size=0):
+        """Initialisation de size"""
+        self.size = size
+
+    def area(self):
+        """Retourne l'aire du carré"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter pour récupérer la taille du carré"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter pour modifier la taille du carré"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
