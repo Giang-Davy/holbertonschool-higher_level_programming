4d9073c29e11e56899a59e6efdbe3e4c7b0d173c
Giang-Davy <davy.dangvu@gmail.com>
2024-11-27 10:15:39 +0000

    0-V2

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-
+"""Definir la fonction"""
 def add_integer(a, b=98):
+    """Erreur pour si != int et float"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    """si float => int"""
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
+
     return (a + b)
