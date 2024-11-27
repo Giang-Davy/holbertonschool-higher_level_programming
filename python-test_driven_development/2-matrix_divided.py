00b8d6c13439d335d4d98b7f1d9509afba26a136
Giang-Davy <davy.dangvu@gmail.com>
2024-11-27 14:48:35 +0000

    2-V5

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-def matrix_divided(matrix, div):
-    """Divise chaque élément de la matrice par div, arrondi à deux décimales"""
+"""Fonction"""
 
+
+def matrix_divided(matrix, div):
+    """Divise chaque élément de la matdeux décimales"""
     # Vérifier si chaque ligne a la même taille
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
-
-    # Faire les calculs ensuite
     return [[round(elem / div, 2) for elem in row] for row in matrix]
