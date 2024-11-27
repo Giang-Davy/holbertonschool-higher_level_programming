6c44e316bcc044af8a3299075af1e5abf353db21
Giang-Davy <davy.dangvu@gmail.com>
2024-11-27 12:15:38 +0000

    0-V6

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """Fonction"""
+
+
 def add_integer(a, b=98):
-    """Ajoute deux entiers ou flottants. Vérifie que les arguments sont de type int ou float."""
+    """Ajoute deux entiers ou flottants"""
     
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-    
     result = a + b
     if result > 1e308:
-        return float('inf') 
-    
+        return float('inf')
     return result
