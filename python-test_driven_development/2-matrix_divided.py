6239f926266310fe789f8b85e49ff976785ba829
Giang-Davy <davy.dangvu@gmail.com>
2024-11-27 14:47:28 +0000

    2-V4

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Fonction de division de matrice"""
-
-
 def matrix_divided(matrix, div):
-    """Divise chaque élément de la matrice par décimales"""  
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    """Divise chaque élément de la matrice par div, arrondi à deux décimales"""
+
+    # Vérifier si chaque ligne a la même taille
+    row_length = len(matrix[0])
     for row in matrix:
-        for elem in row:
-            if not isinstance(elem, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    # Faire les calculs ensuite
     return [[round(elem / div, 2) for elem in row] for row in matrix]
