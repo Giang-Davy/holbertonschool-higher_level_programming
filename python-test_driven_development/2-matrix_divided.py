0b2b1b25a5d5821d9f8ed26956d5e827d888e54e
Giang-Davy <davy.dangvu@gmail.com>
2024-11-27 15:24:26 +0000

    2-V10

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Fonction de division de matrice"""
 
+
 def matrix_divided(matrix, div):
-    """Divise chaque éléme2 décimales"""
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    """Divise chaque élément à 2 décimales"""
+    if not isinstance(matrix, list) or not all(
+            isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not all(isinstance(element, (int, float)) for row in matrix for element in row):
+    if not all(isinstance(element, (int, float)) for row in matrix
+               for element in row):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     row_length = len(matrix[0])
     for row in matrix:
