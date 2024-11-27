ec104843efc27fbcb269a5949e0166e9fc123f98
Giang-Davy <davy.dangvu@gmail.com>
2024-11-27 16:34:40 +0000

    2-V3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,9 +3,9 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Divise chaque élément à 2 décim"""
-    if not isinstance(last_name, str):
-        raise TypeError("first_name must be a string")
+    """Divise chaque élément à 2 d�"""
     if not isinstance(first_name, str):
         raise TypeError("last_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("first_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
