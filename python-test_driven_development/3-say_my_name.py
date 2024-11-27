74c10388901ffcd69c6d289ae2cfe38abb7f62c1
Giang-Davy <davy.dangvu@gmail.com>
2024-11-27 16:29:34 +0000

    ajout 2

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fonction de division de matrice"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Divise chaque élément à 2 décim"""
+    if not isinstance(last_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {}{}".format(first_name, last_name))
