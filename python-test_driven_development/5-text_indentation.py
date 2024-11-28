b3b37da9a3e247f337168fa2e225b768cf97ee5f
Giang-Davy <davy.dangvu@gmail.com>
2024-11-28 10:52:24 +0000

    ajout 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Fonction de division de matrice"""
+
+
+def text_indentation(text):
+    """Fonction pour ajouter des sauts de ligne"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")    
+    text = text.strip()
+    result = ""
+    skip_space = False
+    for i in text:
+        if skip_space and i == " ":
+            continue
+        result += i
+        if i in ".:?":
+            result += "\n"
+            skip_space = True
+        else:
+            skip_space = False
+    print(result.strip(), end="")
