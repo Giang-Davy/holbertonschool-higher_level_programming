ba5a668a2cdbcbc8a42ec6eaf228ef0ebab0dfd5
Giang-Davy <davy.dangvu@gmail.com>
2024-11-28 12:12:55 +0000

    5-V2

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,3 +5,16 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+
+    def test_max_at_the_end(self):
+        self.assertEqual([1,2,3,4])4)
+    def test_max_at_the_beginning(self):
+        self.assertEqual([4,1,2,3])4)
+    def test_max_in_the_middle(self):
+        self.assertEqual([1,2,4,3])4)
+    def test_one_negative_number_in_the_list(self):
+        self.assertEqual([-1,2,3,4])4)
+    def test_only_negative_numbers_in_the_list(self):
+        self.assertEqual([-1,-2,-3,-4])-1)
+    def test_list_of_one_element(self):
+        self.assertEqual([1])1)
