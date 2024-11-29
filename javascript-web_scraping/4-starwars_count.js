729f5c22581662e8eb02882fc28516b3088741ba
Giang-Davy <davy.dangvu@gmail.com>
2024-11-29 14:25:23 +0000

    4V9

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,22 @@
-#!/usr/bin/node
-
-const fetch = require('node-fetch');
-
-const url = process.argv[2];
-
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    console.log('Data:', data);  // Affiche la réponse complète de l'API
     let count = 0;
-    data.results.forEach(film => {
-      if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-        count++;
-      }
-    });
-    console.log(count);
-  })
-  .catch(error => console.error(error));
+
+    if (data.results) {
+      data.results.forEach(film => {
+        console.log(film.characters);  // Affiche la liste des personnages pour chaque film
+        if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+          count++;
+        }
+      });
+    }
+
+    console.log(count);  // Affiche le nombre de films où Wedge Antilles apparaît
+  } else {
+    console.log('Error:', response.statusCode);
+  }
+});
