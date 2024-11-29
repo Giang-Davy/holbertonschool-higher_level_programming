4c10bc2752554f05336dcc396dc28524542e5702
Giang-Davy <davy.dangvu@gmail.com>
2024-11-29 14:29:38 +0000

    4V111

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,16 @@
 #!/usr/bin/node
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else if (response.statusCode === 200) {
-    const data = JSON.parse(body);
-    console.log('Data:', data);
-    let count = 0;
 
-    if (data.results) {
-      data.results.forEach(film => {
-        console.log(film.characters);
-        if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-          count++;
-        }
-      });
-    }
+const fetch = require('node-fetch');
 
+const apiUrl = process.argv[2];
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const characterUrl = 'https://swapi-api.hbtn.io/api/people/18/';
+    const count = data.results.filter(film => film.characters.includes(characterUrl)).length;
     console.log(count);
-  } else {
-    console.log('Error:', response.statusCode);
-  }
-});
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
