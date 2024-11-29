80c5c7c657edd0176c73e395af280c844918d02d
Giang-Davy <davy.dangvu@gmail.com>
2024-11-29 11:41:03 +0000

    5V2

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,17 +1,22 @@
 -- 5-full_table.sql
 SELECT
     table_name AS 'Table',
-    CONCAT('CREATE TABLE `', table_name, '` (', GROUP_CONCAT(
-        CONCAT('`', column_name, '` ', column_type, 
-            IF(is_nullable = 'NO', ' NOT NULL', '')), 
-        SEPARATOR ', '), ') ENGINE=', engine, 
-        ' DEFAULT CHARSET=', table_collation) AS 'Create Table'
+    CONCAT('CREATE TABLE `', table_name, '` (',
+           GROUP_CONCAT(
+               CONCAT('`', column_name, '` ', column_type, 
+                      IF(is_nullable = 'NO', ' NOT NULL', ''),
+                      IF(column_default IS NOT NULL, 
+                         CONCAT(' DEFAULT ', column_default), '')),
+               SEPARATOR ', '),
+           ', PRIMARY KEY(`id`)) ENGINE=', engine,
+           ' DEFAULT CHARSET=', table_collation, 
+           ' COLLATE=', table_collation) AS 'Create Table'
 FROM
-    information_schema.tables 
-    JOIN information_schema.columns 
+    information_schema.tables
+    JOIN information_schema.columns
     ON tables.table_name = columns.table_name
 WHERE
     tables.table_schema = DATABASE()
     AND tables.table_name = 'first_table'
 GROUP BY
-tables.table_name;
+    tables.table_name;
