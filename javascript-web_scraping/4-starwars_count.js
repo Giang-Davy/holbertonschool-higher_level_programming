8ca669fb6d1d4be55e9e43963a1de27f5a252e6c
Giang-Davy <davy.dangvu@gmail.com>
2024-11-29 13:54:42 +0000

    4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body);
+    let count = 0;
+
+    films.results.forEach(film => {
+      if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        count++;
+      }
+    });
+
+    console.log(count);
+  } else {
+    console.log('Error:', response.statusCode);
+  }
+});
