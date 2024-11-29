f5a7dfacd711e38ac50ef6a1fdeed0fb717b0be2
Giang-Davy <davy.dangvu@gmail.com>
2024-11-29 12:21:36 +0000

    0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+// 0-readme.js
+const fs = require('fs');
+
+// Get the file path from the first command-line argument
+const filePath = process.argv[2];
+
+// Use fs.readFile to read the file asynchronously in utf-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);  // If an error occurs, print the error object
+  } else {
+    console.log(data);  // Otherwise, print the file content
+  }
+});
