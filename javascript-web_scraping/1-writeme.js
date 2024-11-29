88630096458a692363327ff87fb724560e3da867
Giang-Davy <davy.dangvu@gmail.com>
2024-11-29 13:36:56 +0000

    1V3

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-// 1-writeme.js
 const fs = require('fs');
 
-// Get the file path and content from the command-line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-// Use fs.writeFile to write the content to the file asynchronously in utf-8 encoding
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
-    console.log(err);  // If an error occurs, print the error object
+    console.log(err);
   }
 });
